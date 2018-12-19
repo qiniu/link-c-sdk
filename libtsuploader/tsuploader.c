b4f96c519bd5d9f1cde7d1f5d12ec2266f3b3f61
lmk <963547817@qq.com>
2018-12-19 18:46:42 +0800

    libtsuploader: change kodo meta

--- libtsuploader/tsuploader.c
+++ libtsuploader/tsuploader.c
@@ -106,11 +106,21 @@ static int linkPutBuffer(const char * uphost, const char *token, const char * ke
                 inttoBCD(pMetas[i].nLength, metaBuf + 15 * i + 10);
         }
         int nMetaValueLen = urlsafe_b64_encode(metaBuf, nMetaLen * 15, metaValue, sizeof(metaValue));
-        nMetaValueLen = snprintf(metaBuf, sizeof(metaBuf), "{\"o\":\"%s\",\"d\":%"PRId64",\"s\":%"PRId64",\"c\":%d}",
-                                 metaValue, duration, seqnum, isDiscontinuity);
+        
+        char **pp = (char **)metaBuf;
+        char *pCnt = metaBuf + sizeof(void *) * 8;
+        
+        pp[0] = pCnt; *pCnt++ = 'o'; *pCnt++ = 0;
+        pp[1] = pCnt; memcpy(pCnt, metaValue, nMetaValueLen); pCnt += (nMetaValueLen+1); *pCnt++ = 0;
+        pp[2] = pCnt; *pCnt++ = 'd'; *pCnt++ = 0;
+        pp[3] = pCnt; pCnt += (sprintf(pCnt, "%"PRId64"", duration) + 1); *pCnt++ = 0;
+        pp[4] = pCnt; *pCnt++ = 's'; *pCnt++ = 0;
+        pp[5] = pCnt; pCnt += (sprintf(pCnt, "%"PRId64"", seqnum) + 1); *pCnt++ = 0;
+        pp[6] = pCnt; *pCnt++ = 'c'; *pCnt++ = 0;
+        pp[7] = pCnt; pCnt += (sprintf(pCnt, "%d", isDiscontinuity) + 1); *pCnt++ = 0;
         
         LinkPutret putret;
-        int ret = LinkUploadBuffer(data, datasize, uphost, token, key, metaBuf, nMetaValueLen, /*mimetype*/NULL, &putret);
+        int ret = LinkUploadBuffer(data, datasize, uphost, token, key, (const char **)pp, 8, /*mimetype*/NULL, &putret);
 
         
 
