dc49925deff69044dd8a1a051856f86b600416f3
lmk <963547817@qq.com>
2018-12-25 09:16:45 +0800

    libtsuploader: modify tsuploader.h

--- libtsuploader/uploader.h
+++ libtsuploader/uploader.h
@@ -22,21 +22,6 @@
 int LinkInit();
 
 /**
- * 创建并且启动一个切片上传实例
- *
- * 此函数不是线程安全函数。
- *
- * @param[out] pTsMuxUploader 切片上传实例
- * @param[in]  pAvArg 上传的 audio/video 的格式参数
- * @param[in]  pUserUploadArg 上传需要的参数，用来设置 token,deviceName
- * @return LINK_SUCCESS 成功; 其它值 失败
- */
-int LinkCreateAndStartAVUploader(OUT LinkTsMuxUploader **pTsMuxUploader,
-                                 IN LinkMediaArg *pAvArg,
-                                 IN LinkUserUploadArg *pUserUploadArg
-                                 );
-
-/**
  * 创建并且启动一个切片上传实例, 带图片上传功能
  *
  * 此函数不是线程安全函数。
@@ -111,14 +96,6 @@ int LinkSetTsType(IN LinkTsMuxUploader *pTsMuxUploader,IN SessionMeta *metas);
  * @return LINK_SUCCESS 成功; 其它值 失败
  */
 void LinkClearTsType(IN LinkTsMuxUploader *pTsMuxUploader);
-
-/**
- * 获取上传实例的缓存 buffer 大小
- *
- * @param[in] pTsMuxUploader 切片上传实例
- * @return size > 0 上传实例的缓存 buffer 大小, <= 0 参照错误码
- */
-int LinkGetUploadBufferUsedSize(IN LinkTsMuxUploader *pTsMuxUploader);
 
 /**
  * 推送视频流数据
